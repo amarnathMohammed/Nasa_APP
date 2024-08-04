63cefb3b1826889e29d7bbd9e4f6eee7957bcc66
MohammedCodes <mohammedcodes3110@gmail.com>
2024-08-04 20:10:39 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function Main(props) {
-    const {data} = props
+  const { data } = props;
+
   return (
-        <div className='imgContainer'>
-            <img src={data.hdurl} alt={data.title || "bg-img"} className='bgImage' />
-        </div>
-        
-    
-  )
+    <div className='imgContainer'>
+      {data.media_type === 'image' ? (
+        <img src={data.url} alt={data.title || 'bg-img'} className='bgImage' />
+      ) : data.media_type === 'video' ? (
+        <iframe
+          src={data.url}
+          title={data.title || 'bg-video'}
+          className='bgVideo'
+          frameBorder='0'
+          allowFullScreen
+        ></iframe>
+      ) : (
+        <p>Unsupported media type</p>
+      )}
+    </div>
+  );
 }
+
