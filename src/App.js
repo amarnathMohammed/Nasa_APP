c6983c28493ea8be6999c9d75a5791e0d6e0c35f
MohammedCodes <mohammedcodes3110@gmail.com>
2024-08-04 19:46:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 function App() {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+ 
 
   const [showModal, setShowModal] = useState(false);
 
